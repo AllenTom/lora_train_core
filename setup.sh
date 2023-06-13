ac9a4b271cd12409322a76e0fcbd92283eb7df9c
AllenTom <qweasdzxczse2@gmail.com>
2023-06-13 11:02:26 +0800

    add install script

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "{ \"event\": \"install-start\" }"
+if [ ! -d "venv" ]; then
+    echo "{ \"event\": \"create-venv\" }"
+    python -m venv venv
+else
+    echo "venv folder already exists, skipping creation..."
+fi
+echo "{ \"event\": \"activate-venv\" }"
+source venv/bin/activate
+echo "{ \"event\": \"install-requirements\" }"
+if pip install --use-pep517 --upgrade -r requirements_macos.txt; then
+    echo "Successfully installed requirements"
+    echo "{ \"event\": \"install-success\" }"
+    exit 0
+else
+    echo "Failed to install requirements"
+    echo "{ \"event\": \"install-failure\" }"
+    exit 1
+fi

ecd55daadba58e345d2b424d7fe170d431168a6e
AllenTom <qweasdzxczse2@gmail.com>
2023-08-13 11:38:42 +0800

    add doctor script to check deps installed

--- doctor_cli.py
+++ doctor_cli.py
@@ -2,8 +2,6 @@ import argparse
 
 from modules import doctor
 
-# Read the requirements.txt file
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train script checker')
 

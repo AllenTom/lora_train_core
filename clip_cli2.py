c6da05a9822a3590c80013541e799b829de6013e
AllenTom <qweasdzxczse2@gmail.com>
2023-09-22 12:33:00 +0800

    add new clip tagger

---
+++ clip_cli2.py
@@ -0,0 +1,56 @@
+import os
+import sys
+
+
+import argparse
+import base64
+import json
+
+from PIL import Image
+
+from deepbooru_cli import get_image_list_from_dir
+from modules import cliptagger2
+
+
+def process():
+    # Create the parser
+    parser = argparse.ArgumentParser()
+    # Add a flag argument
+    parser.add_argument('--image', action='store')
+    parser.add_argument('--input_base64', action='store')
+    parser.add_argument('--dir', action='store')
+    parser.add_argument('--threshold', action='store', default=float(0.7), type=float)
+    parser.add_argument('--with_rank', action='store_true')
+    parser.add_argument('--per', action='store_true')
+    parser.add_argument('--no_result', action='store_false')
+    # Parse the arguments
+    args = parser.parse_args()
+    model = cliptagger2.model.load()
+
+    srcs = []
+    if args.dir:
+        srcs = get_image_list_from_dir(args.dir)
+    else:
+        srcs = [args.image]
+    if srcs is None:
+        print("No images found")
+        return
+    result = []
+    if args.input_base64:
+        # decode
+        decoded_bytes = base64.b64decode(args.input_base64)
+        decoded_string = decoded_bytes.decode('utf-8')
+        input_obj = json.loads(decoded_string)
+        if "files" in input_obj:
+            srcs = input_obj["files"]
+
+    for src in srcs:
+        use_image = Image.open(src)
+        tag_str = model.interrogate(use_image)
+        print(json.dumps({
+            "filename": os.path.basename(src),
+            "tags": tag_str
+        },ensure_ascii=False))
+
+if __name__ == '__main__':
+    process()
